1439d6e6318674579ac81f5dc4d580ec72c9f7a4
Dmitry Ilyin <dilyin@mirantis.com>
2014-02-04 21:08:09 +0400

    Continue development
    Change-Id: I773ef3ec339b9c007b53e2ad0c1833963d96bf51

--- tasks/mysql/simple/site.pp
+++ tasks/mysql/simple/site.pp
@@ -1,3 +1,10 @@
+$fuel_settings = parseyaml($astute_settings_yaml)
+$keystone_hash = $fuel_settings['keystone']
+
+$db_user     = 'keystone'
+$db_password = $keystone_hash['db_password']
+$db_name     = 'keystone'
+
 $use_syslog = true
 
 class { 'mysql::server' :
@@ -8,3 +15,11 @@ class { 'mysql::server' :
 }
 
 class { 'mysql::server::account_security': }
+
+class { 'keystone::db::mysql':
+  user          => $db_user,
+  password      => $db_password,
+  dbname        => $db_name,
+  allowed_hosts => ['localhost', '%'],
+  charset       => 'utf8',
+}
